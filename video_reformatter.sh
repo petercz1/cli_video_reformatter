7a5126d248ad1eb72ea5e81621948473431cef02
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 18:06:29 +0800

    You worry too much about your job.  Stop it.  You are not paid enough to worry.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -7,7 +7,7 @@ function process_files(){
 	video=$(mediainfo --Inform="Audio;%Codec%" $file)
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
-	echo $video
+	echo $filename$video
 	# mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
 	# ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 18 -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
