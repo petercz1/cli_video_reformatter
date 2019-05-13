de6e1aebf02dbf13ec2a2b9b8071247fbb339423
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 18:25:07 +0800

    Don't feed the bats tonight.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -4,14 +4,11 @@
 
 function process_files(){
 	file="$1"
-	video=$(mediainfo --Inform="Video;%Codec%" $file)
-	audio=$(mediainfo --Inform="Audio;%Codec%" $file)
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	echo $filename ": " $video ", " $audio
-
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
-	# ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
+	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
 
 export -f process_files
