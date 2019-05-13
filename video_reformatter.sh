332fa2ecd677b19c51edda290de0379f8e787f47
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 18:01:55 +0800

    A visit to a fresh place will bring strange work.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -5,10 +5,10 @@
 function process_files(){
 	file="$1"
 	video=mediainfo --Language=raw --Inform="Video;%Codec%" $file
-	ech
+	echo $video
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
-	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
+	//mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
 	# ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 18 -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
 
