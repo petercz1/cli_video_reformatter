1ab049cba7ac2bfd9325ea856a5ec98e68698d4a
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 18:01:34 +0800

    The whole world is a tuxedo and you are a pair of brown shoes. 		-- George Gobel

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -4,7 +4,7 @@
 
 function process_files(){
 	file="$1"
-	video=mediainfo --Language=raw --Inform=""
+	video=mediainfo --Language=raw --Inform="Video;%Codec"
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
