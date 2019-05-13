b3b5426d46fac193ced0b15f5092f3b843711535
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 18:01:12 +0800

    Q:	What's a WASP's idea of open-mindedness? A:	Dating a Canadian.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -4,7 +4,7 @@
 
 function process_files(){
 	file="$1"
-	video --Language=raw
+	video=mediainfo --Language=raw --
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
