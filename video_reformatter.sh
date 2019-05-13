a74fca54ee89e704b90163b5ccb9abca5f95482c
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 18:25:00 +0800

    Your nature demands love and your happiness depends on it.

---
+++ video_reformatter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# note: uses GNU parallel
+
+function process_files(){
+	file="$1"
+	video=$(mediainfo --Inform="Video;%Codec%" $file)
+	audio=$(mediainfo --Inform="Audio;%Codec%" $file)
+	extension="${file##*.}"                     	# get the extension
+	filename="${file%.*}"                       	# get the filename
+	echo $filename ": " $video ", " $audio
+
+	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
+	# ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
+}
+
+export -f process_files
+
+find . -type f -iregex ".*/.*\.\(webm\|avi\|mp4\|mkv\|wmv\|flv\|wav\|vob\)" | parallel process_files
