c1e57be801938663d60166eab3c0cb104c035b13
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 19:19:11 +0800

    Q:	What do you call 15 blondes in a circle? A:	A dope ring.
    Q:	Why do blondes put their hair in ponytails?
    A:	To cover up the valve stem.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -7,7 +7,7 @@ function process_files(){
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
-	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
+	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow scale=-1:720 -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
 
 export -f process_files
