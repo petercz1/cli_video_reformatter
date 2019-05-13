1b2d6a92c14e6bdaed17cdd17a7fef20c4bb0cb5
petercz1 <email.peter.carroll@gmail.com>
2019-05-13 17:36:31 +0800

    You will receive a legacy which will place you above want.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -7,7 +7,7 @@ function process_files(){
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
-	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
+	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 18 -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
 
 export -f process_files
