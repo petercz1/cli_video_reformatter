f09beecc8d9ac6c035873a41491fc85fe18fde18
petercz1 <email.peter.carroll@gmail.com>
2019-05-14 11:30:58 +0800

    Advancement in position.

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -7,9 +7,9 @@ function process_files(){
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
-	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset fast -vf scale=-1:480 -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
+	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
 
 export -f process_files
 
-find . -type f -iregex ".*/.*\.\(webm\|avi\|mp4\|mkv\|wmv\|flv\|wav\|vob\)" | time parallel process_files
+find . -type f -iregex ".*/.*\.\(webm\|avi\|mp4\|mkv\|wmv\|flv\|wav\|vob\)" | parallel --joblog --bar log process_files
