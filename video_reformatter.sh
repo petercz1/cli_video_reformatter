ce26540519afec4bfc340f8aa39154292ca45da1
petercz1 <email.peter.carroll@gmail.com>
2019-05-14 09:57:13 +0800

    The first thing we do, let's kill all the lawyers. 		-- Wm. Shakespeare, "Henry VI", Part IV

--- video_reformatter.sh
+++ video_reformatter.sh
@@ -7,9 +7,9 @@ function process_files(){
 	extension="${file##*.}"                     	# get the extension
 	filename="${file%.*}"                       	# get the filename
 	mv "$file" "${filename}.oldfile.${extension}"    # rename file by moving it
-	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset veryslow -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
+	ffmpeg -y -i "${filename}.oldfile.${extension}" -c:v libx264 -crf 23 -preset fast -c:a aac -brand mp41 "$filename".mp4 && rm "${filename}.oldfile.${extension}"
 }
 
 export -f process_files
 
-find . -type f -iregex ".*/.*\.\(webm\|avi\|mp4\|mkv\|wmv\|flv\|wav\|vob\)" | parallel process_files
+find . -type f -iregex ".*/.*\.\(webm\|avi\|mp4\|mkv\|wmv\|flv\|wav\|vob\)" | time parallel process_files
